3265d3751a92116413f2aff8a53337efe3227105
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-07-31 19:03:40 +0600

    2nd task solved

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let cookies = document.getElementById('cookie');
+let counter = document.getElementById('clicker__counter');
+let speed = document.getElementById('clicker__speed');
+let lastClick = Date.now();
+
+function changeSize() {
+	++counter.textContent;
+
+	let newClick = Date.now();
+	speed.textContent = (1000 / (newClick - lastClick)).toFixed(2);
+	lastClick = newClick;
+
+//По просьбам мужа-сладкоежки печенька будет непрерывно увеличиваться, пока не дорастёт до полного размера изображения =)
+	if (cookies.width <= 1024) {
+		cookies.width = cookies.width + 10;
+	} else {
+		cookies.width = 200;
+	}
+}
+
+cookies.onclick = changeSize;
