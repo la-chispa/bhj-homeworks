78ee86a6d576d6b09acdb135c6fe060ffa2324f6
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-03 00:13:49 +0600

    3rd task solved. Advanced

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,16 +3,13 @@
 const slides = Array.from(document.getElementsByClassName('slider__item'));
 const next = document.querySelector('.slider__arrow_next');
 const prev = document.querySelector('.slider__arrow_prev');
+const dots = Array.from(document.getElementsByClassName('slider__dot'));
 
 let currentNumber = 0;
 let nextNumber = 0;
 
 next.onclick = () => {
-	for (let i = 0; i < slides.length; i++) {
-		if (slides[i].className === 'slider__item slider__item_active') {
-			currentNumber = i;
-		}
-	}
+	currentNumber = findCurrentSlide();
 	nextNumber = currentNumber + 1;
 	if (currentNumber >= slides.length - 1) {
 		nextNumber = 0;
@@ -21,16 +18,28 @@ next.onclick = () => {
 }
 
 prev.onclick = () => {
-	for (let i = slides.length - 1; i >= 0; i--) {
-		if (slides[i].className === 'slider__item slider__item_active') {
-			currentNumber = i;
-		}
-	}
+	currentNumber = findCurrentSlide();
 	nextNumber = currentNumber - 1;
 	if (currentNumber === 0) {
 		nextNumber = slides.length - 1;
 	}
 	slideImage();
+}
+
+for (let i = 0; i < dots.length; i++) {
+	dots[i].onclick = () => {
+		currentNumber = findCurrentSlide();
+		nextNumber = i;
+		slideImage();
+	}
+}
+
+function findCurrentSlide() {
+	for (let i = 0; i < slides.length; i++) {
+		if (slides[i].className === 'slider__item slider__item_active') {
+			return i;
+		}
+	}
 }
 
 function slideImage() {
