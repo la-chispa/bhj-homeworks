37f93a359bd4e6d6db7e5247890b71b68a6ea17c
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-02 22:54:38 +0600

    3rd task solved. Update

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,35 +1,39 @@
 'use strict';
+
 const slides = Array.from(document.getElementsByClassName('slider__item'));
 const next = document.querySelector('.slider__arrow_next');
 const prev = document.querySelector('.slider__arrow_prev');
 
+let currentNumber = 0;
+let nextNumber = 0;
 
 next.onclick = () => {
-	let x = 0;
 	for (let i = 0; i < slides.length; i++) {
 		if (slides[i].className === 'slider__item slider__item_active') {
-			x = i;
+			currentNumber = i;
 		}
 	}
-	let y = x + 1;
-	if (x >= slides.length - 1) {
-		y = 0;
+	nextNumber = currentNumber + 1;
+	if (currentNumber >= slides.length - 1) {
+		nextNumber = 0;
 	}
-	slides[x].className = 'slider__item';
-	slides[y].className = 'slider__item slider__item_active';
+	slideImage();
 }
 
 prev.onclick = () => {
-	let x = 0;
 	for (let i = slides.length - 1; i >= 0; i--) {
 		if (slides[i].className === 'slider__item slider__item_active') {
-			x = i;
+			currentNumber = i;
 		}
 	}
-	let y = x - 1;
-	if (x === 0) {
-		y = slides.length - 1;
+	nextNumber = currentNumber - 1;
+	if (currentNumber === 0) {
+		nextNumber = slides.length - 1;
 	}
-	slides[x].className = 'slider__item';
-	slides[y].className = 'slider__item slider__item_active';
-}		
+	slideImage();
+}
+
+function slideImage() {
+	slides[currentNumber].className = 'slider__item';
+	slides[nextNumber].className = 'slider__item slider__item_active';
+}
