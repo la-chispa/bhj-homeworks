38eb129109283a61ff4c0225a79acc021cb9d4a3
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-09 09:32:46 +0600

    1st task fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,31 +1,32 @@
-const timer = document.getElementById('timer');
-const time = setInterval(showTime, 1000);
-function showTime() {
-	if (timer.textContent > 0) {
-		timer.textContent = timer.textContent - 1;
-	} else {
+
+'use strict';
+// Задание с повышенным уровнем сложности #1" 
+const timer = document.getElementById('status')
+let timeGetting = prompt('Сколько секунд будем ждать?');
+const time = setInterval(showTimeAgain, 1000);
+
+function showTimeAgain() {
+	let timerAgain = new Date(parseInt(timeGetting) * 1000);	
+	if (parseInt(timeGetting) > 0) {
+		let counter = `${String(timerAgain.getUTCHours()).padStart(2, '0')}:${String(timerAgain.getMinutes()).padStart(2, '0')}:${String(timerAgain.getSeconds()).padStart(2, '0')}`;
+		timer.textContent = counter;
+	} else{
 		clearInterval(time);
-		alert('Вы победили в конкурсе!');
+		alert('Вы очень терпеливы!\nДержите приз =)');
 		location = 'https://steelinside.com/downloads/Rainbow-Over-Hyderabad.zip';
 	}
+	--timeGetting;
 }
 
-/* Это должно было быть доп. задание "Повышенный уровень сложности #1". 
-Но я споткнулась на том, чтобы заставить числа быть двузначными.
-Если подскажете, буду благодарна.*/
-// const timer2 = document.getElementById('status')
-// let timeGetting = prompt('Сколько секунд будем ждать?');
-// const time2 = setInterval(showTimeAgain, 1000);
-
-// function showTimeAgain() {
-// 	let timerAgain = new Date(parseInt(timeGetting) * 1000);	
-// 	if (parseInt(timeGetting) > 0){
-// 		let counter = `${timerAgain.getUTCHours()}:${timerAgain.getMinutes()}:${timerAgain.getSeconds()}`;
-// 		timer2.textContent = counter;
-// 	} else{
-// 		clearInterval(time2);
-// 		alert('Вы очень терпеливы!\nДержите приз =)');
+// Стандартное задание
+// const timer = document.getElementById('timer');
+// const time = setInterval(showTime, 1000);
+// function showTime() {
+// 	if (timer.textContent > 0) {
+// 		timer.textContent = timer.textContent - 1;
+// 	} else {
+// 		clearInterval(time);
+// 		alert('Вы победили в конкурсе!');
 // 		location = 'https://steelinside.com/downloads/Rainbow-Over-Hyderabad.zip';
 // 	}
-// 	--timeGetting;
 // }
