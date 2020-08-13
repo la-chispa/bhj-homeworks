405326adf530a3ab12ed9abec21abbe387b7e4f3
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-13 10:18:33 +0600

    2nd task fixed. Again

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,25 +1,26 @@
 'use strict';
 const links = document.querySelectorAll('.menu__link');
-const subMenu = Array.from(document.querySelectorAll('.menu_sub'));
 
 for (let i = 0; i < links.length; i++) {
 	links[i].onclick = function() {
 		let menu = this.closest('.menu__item');
 		if (menu.querySelector('.menu_sub').classList.contains('menu_active')) {
 			menu.querySelector('.menu_sub').classList.remove('menu_active');
-		} else {
-			hideSubMenu();
-			if (menu.querySelector('.menu_sub')) {
-				menu.querySelector('.menu_sub').classList.add('menu_active');
-				return false;
-			}
+			return false;
+		} 
+		hideSubMenu();
+		if (menu.querySelector('.menu_sub')) {
+			menu.querySelector('.menu_sub').classList.add('menu_active');
+			return false;
+
 		}
 	}
 }
 
 function hideSubMenu() {
-	for (let subMenuItem in subMenu) {
-		subMenu[subMenuItem].className = 'menu menu_sub';
+	let subMenu = Array.from(document.querySelectorAll('.menu_sub')).find(i => i.classList.contains('menu_active'));
+	if (subMenu) {
+		subMenu.classList.remove('menu_active');
 	}
 }
 
