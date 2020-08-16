735d0b3deef6416bd09e8ef2259d4ae28d4886f8
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-16 09:47:49 +0600

    2nd task solved. Easiest level

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+    document.addEventListener('keydown', (event) => {
+      if (event.key === this.currentSymbol.textContent) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
