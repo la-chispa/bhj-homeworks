abf4f01d80ce72865109386aa68771126820276b
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-08-16 09:48:37 +0600

    3rd task updated

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,14 +3,15 @@ const tabs = Array.from(document.getElementsByClassName('tab'));
 const contents = Array.from(document.getElementsByClassName('tab__content'));
 
 for (let tab of tabs) {
-	tab.onclick = function() {
-		activate(tab);
-	}
+	tab.addEventListener('click', activate)
 }
 
-function activate(tab) {
-	tabs.forEach(tab => tab.classList.remove('tab_active'));
-	contents.forEach(content => content.classList.remove('tab__content_active'));
-	tab.classList.add('tab_active');
-	contents[tabs.indexOf(tab)].classList.add('tab__content_active');
+function activate() {
+	if (this.classList.contains('tab_active')) {
+		return false;
+	}
+	tabs.find(i => i.classList.contains('tab_active')).classList.remove('tab_active');
+	contents.find(i => i.classList.contains('tab__content_active')).classList.remove('tab__content_active');
+	this.classList.add('tab_active');
+	contents[tabs.indexOf(this)].classList.add('tab__content_active');
 }
